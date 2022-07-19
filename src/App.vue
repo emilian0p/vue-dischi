9d408613753486788fb418ffeaf6baef3184fba3
emilian0p <102601927+emilian0p@users.noreply.github.com>
2022-07-19 18:06:29 +0200

    header

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <HeaderContent />
+        <MainContent />
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderContent from './components/HeaderContent.vue';
+import MainContent from './components/MainContent.vue';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    components: {
+        HeaderContent,
+        MainContent
+    }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import '~bootstrap/scss/bootstrap.scss';
 </style>
---
+++ src/components/HeaderContent.vue
@@ -0,0 +1,21 @@
+<template>
+    <header>
+        <img src="../assets/spotify-logo.png" alt="">
+    </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+header {
+    background-color: #2e3a46;
+}
+img {
+    height: 3rem;
+    margin: 1rem;
+}
+</style>
