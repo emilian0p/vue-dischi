0b8bccfbbb51eb3c4fa7929bff44e5ff7cb1964f
emilian0p <102601927+emilian0p@users.noreply.github.com>
2022-07-19 18:30:58 +0200

    static albums containers

--- src/components/AlbumContainer.vue
+++ src/components/AlbumContainer.vue
@@ -1,16 +1,26 @@
 <template>
     <div class="container">
-         <DiskCard />
+         <SingleAlbum />
     </div>
 </template>
 
 <script>
-
+import SingleAlbum from './SingleAlbum.vue';
 export default {
-
+    components: {
+        SingleAlbum,
+    },
 }
 </script>
 
 <style lang="scss">
+@import '~bootstrap/scss/bootstrap.scss';
 
+div.container {
+    width: 60%;
+    margin: 0 auto;
+    display: flex;
+    flex-wrap: wrap;
+    gap: 2rem;
+}
 </style>
