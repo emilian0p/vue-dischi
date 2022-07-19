afe41b2a85e29a09a5957c9a85069ee0d20e8cec
emilian0p <102601927+emilian0p@users.noreply.github.com>
2022-07-19 18:15:36 +0200

    album container

---
+++ src/components/AlbumContainer.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="container">
+         <DiskCard />
+    </div>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style lang="scss">
+
+</style>
--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -1,15 +1,23 @@
 <template>
-    <div>
-        
-    </div>
+    <main>
+        <AlbumContainer />
+    </main>
 </template>
 
 <script>
+import AlbumContainer from './AlbumContainer.vue';
+
 export default {
 
+    components: {
+        AlbumContainer,
+    },
 }
 </script>
 
 <style lang="scss">
-
+main {
+    height: 90vh;
+    background-color: #1f2d3b;
+}
 </style>
